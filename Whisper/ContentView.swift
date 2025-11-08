0be864fcba2115c0e2b58709d916fb6c899d377e
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-11-09 06:32:22 +0900

    Initial Commit

---
+++ Whisper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Whisper
+//
+//  Created by  jwkwon0817 on 11/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
